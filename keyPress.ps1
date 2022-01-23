0b9b1af8aa9ab33970a1ee0d1cfc978fc34db279
0x00-Crashes <42604503+0x00-Crashes@users.noreply.github.com>
2022-01-23 23:28:14 +0100

    Update keyPress.ps1

--- keyPress.ps1
+++ keyPress.ps1
@@ -3,7 +3,7 @@ $WShell = New-Object -com "Wscript.Shell"
 while ($true)
 {
   $WShell.sendkeys("{CAPSLOCK}")
-  Start-Sleep -Milliseconds 150
+  Start-Sleep -Milliseconds 500
   $WShell.sendkeys("{CAPSLOCK}")
   Write-Host "." -nonewline
   Start-Sleep -Seconds 30
